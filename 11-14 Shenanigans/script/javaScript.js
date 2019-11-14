33805f833ea97f38ce8100bbf4e1ddc06ab0265c
Lincoln MacKay <lincoln@Lincoln-MBP.local>
2019-11-14 10:11:16 -0800

    Sorry my bad

---
+++ 11-14 Shenanigans/script/javaScript.js
@@ -0,0 +1,143 @@
+let info = [
+    {
+        Title: 'title',
+        thingsToDo: ['thing1', 'thing2', 'thing3', 'thing4'],
+    },
+    {
+        Title: 'title',
+        thingsToDo: ['thing1', 'thing2', 'thing3', 'thing4', 'thing5'],
+    },
+    {
+        Title: 'title',
+        thingsToDo: ['thing1', 'thing2', 'thing3', 'thing4'],
+    },
+    {
+        Title: 'title',
+        thingsToDo: ['thing1', 'thing2', 'thing3', 'thing4'],
+    },
+    {
+        Title: 'title',
+        thingsToDo: ['thing1', 'thing2', 'thing3', 'thing4'],
+    },
+    {
+        Title: 'title',
+        thingsToDo: ['thing1', 'thing2', 'thing3', 'thing4'],
+    },
+    {
+        Title: 'title',
+        thingsToDo: ['thing1', 'thing2', 'thing3', 'thing4'],
+    },
+    {
+        Title: 'title',
+        thingsToDo: ['thing1', 'thing2', 'thing3', 'thing4'],
+    },
+    {
+        Title: 'title',
+        thingsToDo: ['thing1', 'thing2', 'thing3', 'thing4'],
+    },
+    {
+        Title: 'title',
+        thingsToDo: ['thing1', 'thing2', 'thing3', 'thing4'],
+    },
+    {
+        Title: 'title',
+        thingsToDo: ['thing1', 'thing2', 'thing3', 'thing4'],
+    },
+    {
+        Title: 'title',
+        thingsToDo: ['thing1', 'thing2', 'thing3', 'thing4'],
+    },
+    {
+        Title: 'title',
+        thingsToDo: ['thing1', 'thing2', 'thing3', 'thing4'],
+    },
+    {
+        Title: 'title',
+        thingsToDo: ['thing1', 'thing2', 'thing3', 'thing4'],
+    },
+    {
+        Title: 'title',
+        thingsToDo: ['thing1', 'thing2', 'thing3', 'thing4'],
+    },
+    {
+        Title: 'title',
+        thingsToDo: ['thing1', 'thing2', 'thing3', 'thing4'],
+    },
+    {
+        Title: 'title',
+        thingsToDo: ['thing1', 'thing2', 'thing3', 'thing4'],
+    },
+    {
+        Title: 'title',
+        thingsToDo: ['thing1', 'thing2', 'thing3', 'thing4'],
+    },
+    {
+        Title: 'title',
+        thingsToDo: ['thing1', 'thing2', 'thing3', 'thing4'],
+    },
+    {
+        Title: 'title',
+        thingsToDo: ['thing1', 'thing2', 'thing3', 'thing4'],
+    },
+    {
+        Title: 'title',
+        thingsToDo: ['thing1', 'thing2', 'thing3', 'thing4'],
+    },
+    {
+        Title: 'title',
+        thingsToDo: ['thing1', 'thing2', 'thing3', 'thing4'],
+    },
+    {
+        Title: 'title',
+        thingsToDo: ['thing1', 'thing2', 'thing3', 'thing4'],
+    },
+    {
+        Title: 'title',
+        thingsToDo: ['thing1', 'thing2', 'thing3', 'thing4'],
+    },
+];
+
+let anakin = document.getElementById('anakin');
+let resurrect = document.getElementById('resurrect');
+let exampleDiv = document.getElementById('exampleDiv');
+
+
+anakin.addEventListener('click', function () {
+    deleteStuff();
+})
+
+resurrect.addEventListener('click', function (){
+    generateStuff();
+})
+//for (let i = 0; i < info.length; i++){
+
+function deleteStuff() {
+    while (exampleDiv.firstChild) {
+        exampleDiv.removeChild(exampleDiv.firstChild);
+    }
+}
+
+function generateStuff() {
+    deleteStuff();
+    info.forEach(obj => {
+
+        let container = document.createElement('div')
+        container.setAttribute('class', 'container');
+
+        let title = document.createElement('h2');
+        title.innerText = obj.Title;
+
+        let ul = document.createElement('ul');
+        obj.thingsToDo.forEach(ToDo => {
+            let li = document.createElement('li');
+            li.innerText = ToDo;
+            ul.appendChild(li);
+        });
+
+        container.appendChild(title);
+        container.appendChild(ul);
+        exampleDiv.appendChild(container);
+    });
+}
+
+generateStuff();
