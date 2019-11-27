c4924b21dcf3b8341311733e17fedf402233e5b8
Lincoln MacKay <lincoln@Lincoln-MBP.local>
2019-11-27 13:20:15 -0800

    optimizing

--- ReviewWeekGoal2/script/script.js
+++ ReviewWeekGoal2/script/script.js
@@ -6,36 +6,20 @@ let doTheMath = document.getElementById('doTheMath');
 
 doTheMath.addEventListener('click', function (e) {
     if (operator.value === '+' || operator.value === 'plus' || operator.value === 'add') {
-        addition(firstNum.value, secondNum.value);
+        makePTag(firstNum.value+secondNum.value);
     } else if (
         operator.value === '-' || operator.value === 'minus' || operator.value === 'subtract') {
-        subtraction(firstNum.value, secondNum.value);
+        makePTag(firstNum.value-secondNum.value);
     } else if (
         operator.value === '*' || operator.value === 'times' || operator.value === 'multiply') {
-        multiplication(firstNum.value, secondNum.value);
+        makePTag(firstNum.value*secondNum.value);
     } else if (
         operator.value === '/' || operator.value === 'divide') {
-        division(firstNum.value, secondNum.value)
+        makePTag(firstNum.value/secondNum.value);
     } else {
         alert('Not a valid operator');
     }
 });
-
-function addition(x, y) {
-    makePTag(x+y);
-}
-
-function subtraction(x, y) {
-    makePTag(x-y);
-}
-
-function multiplication(x, y) {
-    makePTag(x*y);
-}
-
-function division(x, y) {
-    makePTag(x/y);
-}
 
 function makePTag(text) {
     answerField.innerHTML = '';
