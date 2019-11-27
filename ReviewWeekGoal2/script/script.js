d1a76dc41b5dc0917a6e8c1c39f23d4769779872
Lincoln MacKay <lincoln@Lincoln-MBP.local>
2019-11-27 13:33:32 -0800

    Cleaning code.

--- ReviewWeekGoal2/script/script.js
+++ ReviewWeekGoal2/script/script.js
@@ -1,27 +1,28 @@
-let firstNum = document.getElementById('firstNum');
-let secondNum = document.getElementById('secondNum');
-let operator = document.getElementById('operator');
-let answerField = document.getElementById('answerField');
 let doTheMath = document.getElementById('doTheMath');
 
 doTheMath.addEventListener('click', function (e) {
-    if (operator.value === '+' || operator.value === 'plus' || operator.value === 'add') {
-        makePTag(firstNum.value+secondNum.value);
-    } else if (
-        operator.value === '-' || operator.value === 'minus' || operator.value === 'subtract') {
-        makePTag(firstNum.value-secondNum.value);
-    } else if (
-        operator.value === '*' || operator.value === 'times' || operator.value === 'multiply') {
-        makePTag(firstNum.value*secondNum.value);
-    } else if (
-        operator.value === '/' || operator.value === 'divide') {
-        makePTag(firstNum.value/secondNum.value);
+    //get values when the button is clicked.
+    let firstNum = document.getElementById('firstNum').value;
+    let secondNum = document.getElementById('secondNum').value;
+    let operator = document.getElementById('operator').value;
+
+    //check the operator and do the relevant math and pass it in as a parameter. No need to convert the strings to numbers and back to text.  Javascript is not a strict language.
+    if (operator === '+' || operator === 'plus' || operator === 'add') {
+        makePTag(firstNum + secondNum);
+    } else if (operator === '-' || operator === 'minus' || operator === 'subtract') {
+        makePTag(firstNum - secondNum);
+    } else if (operator === '*' || operator === 'times' || operator === 'multiply') {
+        makePTag(firstNum * secondNum);
+    } else if (operator === '/' || operator === 'divide') {
+        makePTag(firstNum / secondNum);
     } else {
         alert('Not a valid operator');
     }
 });
 
 function makePTag(text) {
+    //get out field, make sure there is nothing in it, and create a tag with the innerText of our parameter.
+    let answerField = document.getElementById('answerField');
     answerField.innerHTML = '';
     let p = document.createElement('p');
     p.innerText = text;
